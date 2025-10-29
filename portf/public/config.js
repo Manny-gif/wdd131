9c9f9eb2b912a0aa66df10c928d5b61fdb848ca7
yasaemmanuel <yas23001@byui.edu>
2025-10-28 18:50:19 -0600

    name1

--- portf/public/config.js
+++ portf/public/config.js
@@ -1,12 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAZ7nZhOS7cr5-AvwmfRtR11HlKRqSos3Q",
-  authDomain: "yourname-portfolio.firebaseapp.com",
-  projectId: "yourname-portfolio",
-  storageBucket: "yourname-portfolio.appspot.com",
-  messagingSenderId: "283694418074",
-  appId: "1:283694418074:web:bdef115a9eec9d077a7723",
-  // measurementId: "G-T7MLFLB8E5"
+  apiKey: "AIzaSyDzNySeu4_9lJrU1X_7Qn3yQWz7MsQXGGw",
+  authDomain: "manny-port.firebaseapp.com",
+  projectId: "manny-port",
+  storageBucket: "manny-port.firebasestorage.app",
+  messagingSenderId: "993639336980",
+  appId: "1:993639336980:web:eb98c314e1d0190b3cab01",
+  measurementId: "G-R3C77TGZJR"
 };
-
-
-  
